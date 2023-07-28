8b5d95854327cc1c0b0377cde6839d9a49ec11f4
L.J Solis <l.jsolis@L.lan>
2023-07-27 20:28:49 -0500

    Made general structure for the js file as far as functions go.

--- cat.js
+++ cat.js
@@ -1,1 +1,3 @@
-let catApiKey = live_MI5rOkbC3LSYXKNirvGsCCR4Fx6RpllLp6FRHY17sSzyUmipdKho6ua8JnkBYbDn
+const catApiKey = live_MI5rOkbC3LSYXKNirvGsCCR4Fx6RpllLp6FRHY17sSzyUmipdKho6ua8JnkBYbDn
+const catUrl = `https://api.thecatapi.com/v1`;
+
